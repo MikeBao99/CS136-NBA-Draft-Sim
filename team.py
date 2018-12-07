aa60112b1e4a37ecad15f29f952dc9ad1e8d2535
Jonathan Huang <jonathanhuang42299@gmail.com>
2018-12-07 15:55:39 -0500

    team format giannis antetouok

---
+++ team.py
@@ -0,0 +1,17 @@
+class Team:
+  def __init__(self, id, power, tank):
+    self.id = id
+    self.power = power
+    self.tank = tank
+
+  def getPower(self):
+    return self.power
+
+  def setPower(self, newPower):
+    self.power = newPower
+
+  def reportPower(self):
+    if self.tank = True:
+      return 0
+    else:
+      return self.power
